7368920aec8835e83acf9f3f3913d60090b49487
Đặng Đức Minh <minhddhe163055@fpt.edu.vn>
2023-06-18 21:50:24 +0700

    add 2 options for the game

--- app/src/main/java/com/example/prm392/GameScreen.java
+++ app/src/main/java/com/example/prm392/GameScreen.java
@@ -2,6 +2,7 @@ package com.example.prm392;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +42,10 @@ public class GameScreen extends AppCompatActivity {
     }
     public void btn4(View view){
         story.selectPosition(story.nextPos4);
+    }
+    public void goToTitle(){
+        Intent titleScreen = new Intent(this, TitleScreen.class);
+        startActivity(titleScreen);
     }
 
 }
--- app/src/main/java/com/example/prm392/Story.java
+++ app/src/main/java/com/example/prm392/Story.java
@@ -14,7 +14,10 @@ public class Story {
     public void selectPosition(String pos){
         switch (pos){
             case "startingPoint": startingPoint(); break;
+            case "weapon": weapon(); break;
             case "stay": stay(); break;
+            case "openChest": openChest(); break;
+            case "goToTitle": gs.goToTitle(); break;
         }
     }
 
@@ -42,24 +45,65 @@ public class Story {
 
     }
 
-    public void stay(){
+    public void weapon(){
 
-        gs.image.setImageResource(R.drawable.disintegrate);
+        gs.image.setImageResource(R.drawable.chest);
 
-        gs.text.setText("You think about this for a moment, and then you remembered you actually got kidnapped by Aliens," +
-                "\nand now ur on their ship. Suddenly, an alien comes into the room to find you and bring you to the lab." +
-                "\nThey do experiments on you and you die from the trauma and pain.");
+        gs.text.setText("You found a box. \n\nMight be something good inside.");
 
-        gs.btn1.setText("Retry");
+        gs.btn1.setText("Open it");
+        gs.btn2.setText("Go back");
+        gs.btn3.setText("");
+        gs.btn4.setText("");
+
+        gs.btn3.setVisibility(View.INVISIBLE);
+        gs.btn4.setVisibility(View.INVISIBLE);
+
+        nextPos1 = "openChest";
+        nextPos2 = "startingPoint";
+        nextPos3 = "";
+        nextPos4 = "";
+
+    }
+
+    public void openChest(){
+        gs.image.setImageResource(R.drawable.raygun);
+
+        gs.text.setText("You found an alien ray gun.\n\nNice, now you can defend yourself.");
+
+        gs.btn1.setText("Take it and go back");
         gs.btn2.setText("");
         gs.btn3.setText("");
         gs.btn4.setText("");
+
         gs.btn2.setVisibility(View.INVISIBLE);
         gs.btn3.setVisibility(View.INVISIBLE);
         gs.btn4.setVisibility(View.INVISIBLE);
 
         nextPos1 = "startingPoint";
         nextPos2 = "";
+        nextPos3 = "";
+        nextPos4 = "";
+    }
+
+    public void stay(){
+
+        gs.image.setImageResource(R.drawable.disintegrate);
+
+        gs.text.setText("You think about this for a moment, and then you remembered you actually got kidnapped by aliens, " +
+                "and now you're on their ship. Suddenly, an alien comes into the room to find you and bring you to the lab. " +
+                "They do experiments on you and you die from the trauma and pain. GAME OVER");
+
+        gs.btn1.setText("Try Again");
+        gs.btn2.setText("Back to Title");
+        gs.btn3.setText("");
+        gs.btn4.setText("");
+
+        gs.btn3.setVisibility(View.INVISIBLE);
+        gs.btn4.setVisibility(View.INVISIBLE);
+
+        nextPos1 = "startingPoint";
+        nextPos2 = "goToTitle";
         nextPos3 = "";
         nextPos4 = "";
 
